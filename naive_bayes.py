361bb4ab0bdd6161c9e5939f755f46ffe94cdded
Cai <35024963+YESAndy@users.noreply.github.com>
2018-10-12 19:03:56 +0800

    Create naive_bayes.py

---
+++ naive_bayes.py
@@ -0,0 +1,34 @@
+import numpy as np
+import random
+from sklearn import cross_validation
+
+class naive_bayes:
+
+    def __init__(self,k,n):
+        self.k=k
+        self.n=n
+        self.x=np.zeros(2,self.n)
+        self.y=np.zeros(self.n)
+        self.c=np.zeros(self.k)
+
+    def read():
+        pass
+    
+    def cal_probab():
+    
+        countc
+        countx1=0
+        countx2=0
+        county=0
+        
+        for i in range(self.n):
+            if 
+        
+        
+    
+    def train():
+        pass
+        
+    def predict():
+        pass
+        
